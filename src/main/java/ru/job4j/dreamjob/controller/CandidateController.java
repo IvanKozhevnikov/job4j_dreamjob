c505c7c759ebb1e7a9c86d059cd2fa148921312b
Ivan_Kozhevnikov <IvanKozhevnikof@yandex.ru>
2023-08-12 14:38:40 +0300

    4. Формы. Загрузка файла на сервер. [#504855]

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -4,10 +4,11 @@ import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.job4j.dreamjob.dto.FileDto;
 import ru.job4j.dreamjob.model.Candidate;
 import ru.job4j.dreamjob.service.CandidateService;
 import ru.job4j.dreamjob.service.CityService;
-import ru.job4j.dreamjob.service.SimpleCandidateService;
 
 @ThreadSafe
 @Controller
@@ -17,8 +18,8 @@ public class CandidateController {
     private final CandidateService candidateService;
     private final CityService cityService;
 
-    public CandidateController(SimpleCandidateService simpleCandidateService, CityService cityService) {
-        this.candidateService = simpleCandidateService;
+    public CandidateController(CandidateService candidateService, CityService cityService) {
+        this.candidateService = candidateService;
         this.cityService = cityService;
     }
 
@@ -35,10 +36,15 @@ public class CandidateController {
     }
 
     @PostMapping("/create")
-    public String create(@ModelAttribute Candidate candidate) {
-        candidateService.save(candidate);
+    public String create(@ModelAttribute Candidate candidate, @RequestParam MultipartFile file, Model model) {
+        try {
+        candidateService.save(candidate, new FileDto(file.getOriginalFilename(), file.getBytes()));
         return "redirect:/candidates";
+    } catch (Exception exception) {
+        model.addAttribute("message", exception.getMessage());
+        return "errors/404";
     }
+}
 
     @GetMapping("/{id}")
     public String getById(Model model, @PathVariable int id) {
@@ -53,9 +59,9 @@ public class CandidateController {
     }
 
     @PostMapping("/update")
-    public String update(@ModelAttribute Candidate candidate, Model model) {
+    public String update(@ModelAttribute Candidate candidate, @RequestParam MultipartFile file, Model model) {
         try {
-            var isUpdated = candidateService.update(candidate);
+            var isUpdated = candidateService.update(candidate, new FileDto(file.getOriginalFilename(), file.getBytes()));
             if (!isUpdated) {
                 model.addAttribute("message", "Вакансия с указанным идентификатором не найдена");
                 return "errors/404";
--- src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
+++ src/main/java/ru/job4j/dreamjob/controller/VacancyController.java
@@ -4,9 +4,10 @@ import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import ru.job4j.dreamjob.dto.FileDto;
 import ru.job4j.dreamjob.model.Vacancy;
 import ru.job4j.dreamjob.service.CityService;
-import ru.job4j.dreamjob.service.SimpleVacancyService;
 import ru.job4j.dreamjob.service.VacancyService;
 
 @ThreadSafe
@@ -17,8 +18,8 @@ public class VacancyController {
     private final VacancyService vacancyService;
     private final CityService cityService;
 
-    public VacancyController(SimpleVacancyService simpleVacancyService, CityService cityService) {
-        this.vacancyService = simpleVacancyService;
+    public VacancyController(VacancyService vacancyService, CityService cityService) {
+        this.vacancyService = vacancyService;
         this.cityService = cityService;
     }
 
@@ -35,9 +36,14 @@ public class VacancyController {
     }
 
     @PostMapping("/create")
-    public String create(@ModelAttribute Vacancy vacancy) {
-        vacancyService.save(vacancy);
-        return "redirect:/vacancies";
+    public String create(@ModelAttribute Vacancy vacancy, @RequestParam MultipartFile file, Model model) {
+        try {
+            vacancyService.save(vacancy, new FileDto(file.getOriginalFilename(), file.getBytes()));
+            return "redirect:/vacancies";
+        } catch (Exception exception) {
+            model.addAttribute("message", exception.getMessage());
+            return "errors/404";
+        }
     }
 
     @GetMapping("/{id}")
@@ -53,18 +59,18 @@ public class VacancyController {
     }
 
     @PostMapping("/update")
-    public String update(@ModelAttribute Vacancy vacancy, Model model) {
+    public String update(@ModelAttribute Vacancy vacancy, @RequestParam MultipartFile file, Model model) {
         try {
-        var isUpdated = vacancyService.update(vacancy);
-        if (!isUpdated) {
-            model.addAttribute("message", "Вакансия с указанным идентификатором не найдена");
+            var isUpdated = vacancyService.update(vacancy, new FileDto(file.getOriginalFilename(), file.getBytes()));
+            if (!isUpdated) {
+                model.addAttribute("message", "Вакансия с указанным идентификатором не найдена");
+                return "errors/404";
+            }
+            return "redirect:/vacancies";
+        } catch (Exception exception) {
+            model.addAttribute("message", exception.getMessage());
             return "errors/404";
         }
-        return "redirect:/vacancies";
-    } catch (Exception e) {
-        model.addAttribute("message", e.getMessage());
-        return "error/404";
-    }
     }
 
     @GetMapping("/delete/{id}")
