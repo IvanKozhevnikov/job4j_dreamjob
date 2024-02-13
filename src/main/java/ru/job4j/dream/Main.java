a657178af63ba63e7c35c1e34f65bd1f43e1971c
Ivan_Kozhevnikov <IvanKozhevnikof@yandex.ru>
2024-02-13 14:02:47 +0300

    3. Filter [#504865]

--- src/main/java/ru/job4j/dream/Main.java
+++ src/main/java/ru/job4j/dream/Main.java
@@ -2,10 +2,49 @@ package ru.job4j.dream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.xmlunit.builder.Input;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 @SpringBootApplication
 public class Main {
-    public static void main(String[] args) {
+
+    private static String loadSysEnvIfNullThenConfig(String sysEnv, String key, Properties config) {
+        String value = System.getenv(sysEnv);
+        if (value == null) {
+            value = config.getProperty(key);
+        }
+        return value;
+    }
+
+    private static Connection loadConnection() throws ClassNotFoundException, SQLException {
+        var config = new Properties();
+        try (InputStream in = Main.class.getClassLoader()
+                .getResourceAsStream("app.properties")) {
+            config.load(in);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+        String url = loadSysEnvIfNullThenConfig("JDBC_URL", "url", config);
+        String username = loadSysEnvIfNullThenConfig("JDBC_USERNAME", "username", config);
+        String password = loadSysEnvIfNullThenConfig("JDBC_PASSWORD", "password", config);
+        String driver = loadSysEnvIfNullThenConfig("JDBC_DRIVER", "driver-class-name", config);
+        System.out.println("url=" + url);
+        Class.forName(driver);
+        return DriverManager.getConnection(
+                url, username, password
+        );
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        try (Connection connection = loadConnection()) {
+            connection.createStatement();
+        }
         SpringApplication.run(Main.class, args);
     }
 }
